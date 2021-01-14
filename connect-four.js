8502c0b48e875e3e2061654cca5b453d538e4fc1
Marshall-Diffey <marshalldiffey@yahoo.com>
2021-01-14 16:45:23 -0600

    Addressed filling columns

--- connect-four.js
+++ connect-four.js
@@ -1,8 +1,24 @@
  let turn = 'red';
 
+ const col0 = document.querySelectorAll('[id$="-0"]');
+ const col1 = document.querySelectorAll('[id$="-1"]');
+ const col2 = document.querySelectorAll('[id$="-2"]');
+ const col3 = document.querySelectorAll('[id$="-3"]');
+ const col4 = document.querySelectorAll('[id$="-4"]');
+ const col5 = document.querySelectorAll('[id$="-5"]');
+
+if (box.id === 'column-0') {
+    // take div at col0[0]; appendChild token to it
+    // .pop col0;
+    // if (col0.length === 1) {
+        // 'column-0' disabled = true
+    //}
+}
+
 const board = document.getElementById('click-targets');
+
 board.addEventListener('click', event => {
-    const box = event.target;
+    const box = event.target //.id  // ${[7]}
     if (turn === 'red') {
         const token = document.createElement('img');
         token.setAttribute('class', 'token');
@@ -18,8 +34,7 @@ board.addEventListener('click', event => {
         box.appendChild(token);
         turn = 'red';
     }
-    
-   
+
 })
 
 //step1.) grab  columns;
@@ -31,7 +46,6 @@ board.addEventListener('click', event => {
 
 //Step3.) when clicked, place token into square with id ending in array[0];
 
-//step4.) .unshift array;
+//step4.) .shift array;
 
 //step5.) when column's array is empty, remove ghost circle divider;
-
