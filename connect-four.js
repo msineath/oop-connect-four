a4f756e67d4a370780189e54ba36e86160e1599a
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-01-14 16:37:54 -0500

    figuring out moves

--- connect-four.js
+++ connect-four.js
@@ -1,20 +1,30 @@
-let turn = 'red';
+ let turn = 'red';
 
-const board = document.getElementById('board-squares');
+const board = document.getElementById('click-targets');
 board.addEventListener('click', event => {
-    const move = event.target.id;
+    const box = event.target;
     if (turn === 'red') {
-        const box = event.target.id;
         const token = document.createElement('img');
         token.setAttribute('class', 'token');
         token.classList.add('red');
         box.appendChild(token);
+        turn = 'black';
     }
     if (turn === 'black') {
-        const box = event.target.id;
+        const box = event.target;
         const token = document.createElement('img');
         token.setAttribute('class', 'token');
         token.classList.add('black');
         box.appendChild(token);
+        turn = 'red';
     }
+    
+    // const target = document.querySelector('[id^="square-0"]').id;
+    // for (let position of target) {
+    //     if (position  = event.target) {
+    //         const ghostCircle = document.getElementById(`column-${box[9]}`)
+            
+    //         console.log(box[9]);
+    //     }
+    // }
 })
